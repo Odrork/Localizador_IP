1a7f828b365b0bbc9c6c83e42bfbfa8bcc4f287e
Odrork <109298575+Odrork@users.noreply.github.com>
2022-12-14 18:32:30 +0100

    Update Buscador_IP.py

--- Buscador_IP.py
+++ Buscador_IP.py
@@ -1,7 +1,10 @@
+
+#Import de geocoder
 import geocoder as geo
 
+#Creación de bucle while.
 while True:
-    ip=input('Introduce IP: ')
+    ip=input('Introduce IP: ') #Entrada de IP
     g = geo.ip(ip)
     print('La IP entrada proviene de {}, {}'.format(g.country,g.city))
     o = input('¿Desea continuar (s/n)?: ')
