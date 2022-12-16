9dc45672060ce9c23bd7600e67201baf70afbe3b
Odrork <109298575+Odrork@users.noreply.github.com>
2022-12-16 20:06:29 +0100

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -6,7 +6,7 @@
         Novedades: 
         
             - Boton aceptar suprimido, remplazado por un entry que devolvera el resultado
-            - Frame creado para poder darle estilo a la ventana
+            - Implementacion de estilo 
 
         ----Odrork----
 """
@@ -16,7 +16,7 @@ from tkinter import messagebox as MessageBox
 import geocoder as geo
 
 #Declaracion variable vacia, utilizada para almacenar la IP entrada.
-IP = '190.111.40.250'
+IP = ''
 
 r= StringVar()
 
@@ -27,29 +27,28 @@ def Localizar (IP):
 #Creacion de la raiz. La raiz es eel contenedor base de todos los widgets que se iran integrando
 root = Tk()
 root.title('Localizador IP')        #Título de la ventana
+root.config(bg='#83837F')
 
-frame = Frame(root)
-frame.config(width=200, height=200, bg='CFCF0C')
 
 #Creacion del primer label de texto
-label = Label(frame, text='Entrada IP')
+label = Label(root, text='Entrada IP')
 label.grid(row=0, column=0, sticky='w', padx=5, pady=5)
 
 #Creación de la entrada de IP por pantalla. 
-entry = Entry(frame)
+entry = Entry(root)
 entry.grid(row=0, column=1, padx=5, pady=5)
 entry.config(justify='rigth', state='normal')
 
 #Label salida del resultado
-Label(frame, text="Localizacion").pack()
+Label(root, text="Localizacion").pack()
 label.grid(row=1,column=0,sticky='w', padx=5, pady=5)
 
 #Impresion del resultado
-Entry(frame, justify="center", textvariable=r, state="disabled").pack()
+Entry(root, justify="center", textvariable=r, state="disabled").pack()
 entry.grid(row=1, column=1, padx=5, pady=5)
 
 #Boton 'Cancelar' 
-buttonCancelar = Button(frame, text='Cerrar', command=root.destroy, justify='center')
+buttonCancelar = Button(root, text='Cerrar', command=root.destroy, justify='center')
 buttonCancelar.place(x=200, y=20)
 buttonCancelar.grid(row=4, column=1, padx=5, pady=5)
 
