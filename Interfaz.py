7c8a9d5fe27fd1c2b1998efc2f4933ef5b2b8422
Odrork <109298575+Odrork@users.noreply.github.com>
2022-12-19 17:33:37 +0100

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -1,24 +1,21 @@
 """
         Interfaz grafica para Localizador de IP.
 
-        Version = 0.2.1
+        Version = 0.2.2
 
         Novedades: 
         
-            - Boton aceptar suprimido, remplazado por un entry que devolvera el resultado
-            - Implementacion de estilo 
+            - Implementacion de la lectura de la IP por el Entry
+            - Cambios en la disposicion de la interfaz
 
         ----Odrork----
 """
-
+import tkinter as tk
 from tkinter import * 
 from tkinter import messagebox as MessageBox
 import geocoder as geo
 
-#Declaracion variable vacia, utilizada para almacenar la IP entrada.
-IP = ''
-
-r= StringVar()
+IP = tk.StringVar()
 
 def Localizar (IP): 
     g = geo.ip(IP)
@@ -27,30 +24,43 @@ def Localizar (IP):
 #Creacion de la raiz. La raiz es eel contenedor base de todos los widgets que se iran integrando
 root = Tk()
 root.title('Localizador IP')        #Título de la ventana
-root.config(bg='#83837F')
+root.config(bg='#83837F')  #Estilo de la ventana
+root.resizable(0,0)
+root.geometry('250x150')
 
+#Label titulo
+label_titulo = Label(root, text='Localizador')
+label_titulo.place(x=92,y=5)
 
-#Creacion del primer label de texto
-label = Label(root, text='Entrada IP')
-label.grid(row=0, column=0, sticky='w', padx=5, pady=5)
+#Label entrada
+label_entrada = Label(root, text='Entrada IP')
+label_entrada.grid(row=1, column=0, sticky='w', padx=10, pady=5)
+label_entrada.place(x=10,y=40)
+label_entrada.config(width=10)
 
 #Creación de la entrada de IP por pantalla. 
-entry = Entry(root)
-entry.grid(row=0, column=1, padx=5, pady=5)
-entry.config(justify='rigth', state='normal')
+entry_entrada = Entry(root, textvariable=IP)
+entry_entrada.grid(row=1, column=1, padx=5, pady=5)
+entry_entrada.config(justify='rigth', state='normal')
+entry_entrada.place(x=100,y=40)
 
 #Label salida del resultado
-Label(root, text="Localizacion").pack()
-label.grid(row=1,column=0,sticky='w', padx=5, pady=5)
+label_localizacion = Label(root, text='Localizacion')
+label_localizacion.grid(row=2,column=0, sticky='w', padx=5, pady=5)
+label_localizacion.place(x=10, y=75)
+label_localizacion.config(width=10)
 
 #Impresion del resultado
-Entry(root, justify="center", textvariable=r, state="disabled").pack()
-entry.grid(row=1, column=1, padx=5, pady=5)
+salida_localizacion = Entry(root, state='readonly') #, justify="center", textvariable=r, state="disabled")
+salida_localizacion.grid(row=2, column=1, padx=5, pady=5)
+salida_localizacion.config(justify='rigth', state='disabled')
+salida_localizacion.place(x=100,y=76)
 
 #Boton 'Cancelar' 
 buttonCancelar = Button(root, text='Cerrar', command=root.destroy, justify='center')
-buttonCancelar.place(x=200, y=20)
-buttonCancelar.grid(row=4, column=1, padx=5, pady=5)
+buttonCancelar.place(x=95, y=110)
+# buttonCancelar.grid(row=3, column=0)
+buttonCancelar.config(width=7,height=1, foreground='#F53A30')
 
-#Loop de la ventana. 
+#Loop de la ventana
 root.mainloop()
