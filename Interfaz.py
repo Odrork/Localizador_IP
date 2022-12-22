8c378b90aed7bcb0de7daac5f247ea4329d1c7b1
Odrork <109298575+Odrork@users.noreply.github.com>
2022-12-22 17:05:31 +0100

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -1,37 +1,36 @@
 """
-        Interfaz grafica para Localizador de IP.
+            Interfaz grafica para Localizador de IP.
 
-        Version = 0.2.3
+            Version = 0.2.3
 
-        Novedades: 
-        
-            - Se ha vuelto a implementar el boton aceptar para hacer llamada a la funcion localizar
-            - Se ha borrado 'from tkinter import *' para evitar problemas en la implementacion del codigo
+            Novedades: 
+            
 
-        Problemas: 
 
-            - Mirar como capturar entrada
-            - Lanzar funcion con boton aceptar
+            Problemas: 
 
-        ----Odrork----
-"""
+                - Poner funcion al boton acceptar, hace que el boton acceptar se borre. 
+                - Buscar manera de asociar funcion al boton o a un Entry y que lo imprima por pantalla
+
+            ----Odrork----
+    """
 import tkinter as tk
 # from tkinter import * 
 from tkinter import messagebox as MessageBox
 import geocoder as geo
 
+#Funcion localizar IP 
 
-
-def Localizar (IP):
-    g = geo.ip(IP)
-    print('La IP entrada proviene de {}, {}'.format(g.country,g.city))
-     
-#Creacion de la raiz. La raiz es eel contenedor base de todos los widgets que se iran integrando
+def Localizar (x):
+    g = geo.ip(x)
+    print('La IP entrada proviene de {}, {}'.format(g.city,g.country))
+    
+#Creacion de la raiz. La raiz es el contenedor base de todos los widgets que se iran integrando
 root = tk.Tk()
 root.title('Localizador IP')        #Título de la ventana
-root.config(bg='#83837F')  #Estilo de la ventana
-root.resizable(0,0)
-root.geometry('250x150')
+root.config(bg='#83837F')       #Estilo de la ventana
+root.resizable(0,0)      #Ventana fija, posibilidad de escarlar tamaño
+root.geometry('250x150')        # Tamaño de ventana
 
 #Label titulo
 label_titulo = tk.Label(root, text='Localizador')
@@ -43,6 +42,7 @@ label_entrada.grid(row=1, column=0, sticky='w', padx=10, pady=5)
 label_entrada.place(x=10,y=40)
 label_entrada.config(width=10)
 
+#Variable para almacenar IP entrada
 IP = tk.StringVar()
 
 #Creación de la entrada de IP por pantalla. 
@@ -51,7 +51,6 @@ entry_entrada.grid(row=1, column=1, padx=5, pady=5)
 entry_entrada.config(justify='rigth', state='normal')
 entry_entrada.place(x=100,y=40)
 
-
 #Label salida del resultado
 label_localizacion = tk.Label(root, text='Localizacion')
 label_localizacion.grid(row=2,column=0, sticky='w', padx=5, pady=5)
@@ -59,13 +58,13 @@ label_localizacion.place(x=10, y=75)
 label_localizacion.config(width=10)
 
 #Impresion del resultado
-salida_localizacion = tk.Entry(root, state='readonly') #, justify="center", textvariable=r)
+salida_localizacion = tk.Entry(root, state='readonly', textvariable=IP) #, justify="center", textvariable=r)
 salida_localizacion.grid(row=2, column=1, padx=5, pady=5)
 salida_localizacion.config(justify='rigth', state='disabled')
 salida_localizacion.place(x=100,y=76)
 
 #Boton 'Aceptar' 
-buttonAceptar = tk.Button(root, text='Aceptar',justify='center', command=Localizar(IP)) #command=Localizar(IP)
+buttonAceptar = tk.Button(root, text='Aceptar',justify='center') #command=Localizar(IP)
 buttonAceptar.place(x=60, y=110)
 buttonAceptar.config(width=7, height=1, foreground='blue')
 
@@ -77,8 +76,3 @@ buttonCancelar.config(width=7,height=1, foreground='#F53A30')
 #Loop de la ventana
 root.mainloop()
     
-
-"""  entry_entrada = tk.Entry(root) #textvariable=IP
-entry_entrada.grid(row=1, column=1, padx=5, pady=5)
-entry_entrada.config(justify='rigth', state='normal')
-entry_entrada.place(x=100,y=40)   """ 
