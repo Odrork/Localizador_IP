98560a5b155986cb9b5ba0a6ceaefb69edfdb449
Odrork <109298575+Odrork@users.noreply.github.com>
2023-01-13 14:23:57 +0100

    Update Buscador_IP.py

--- Buscador_IP.py
+++ Buscador_IP.py
@@ -5,19 +5,21 @@ Devuelve país y cuidad de la IP entrada.
 
 """
 
-
 #Import de geocoder
 import geocoder as geo
+
+# Función localizar IP
+def Localizador(ip): 
+    g = geo.ip(ip)
+    print('La IP entrada proviene de {}, {}'.format(g.country,g.city))
+    print('lat: {}, lon {}'.format(g.lat,g.lng))
+
 
 #Creación de bucle while.
 while True:
     ip=input('Introduce IP: ') #Entrada de IP
-    g = geo.ip(ip)
-    try:
-        print('La IP entrada proviene de {}, {}'.format(g.country,g.city))
-        print('lat: {}, lon {}'.format(g.lat,g.lng))
-        o = input('¿Desea continuar (s/n)?: ')
-    except:
-        print('IP no valida')
+    Localizador(ip)
+    o = input('¿Desea continuar (s/n)?: ')
     if o == 'n': 
         break
+
