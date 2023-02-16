c0ccf29e24787dc2d9ca5fb6eedb9fc25763b436
Odrork <109298575+Odrork@users.noreply.github.com>
2023-02-16 18:02:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import geocoder
 import tkinter as tk
 
-def LocalizacionIP(ip):
+def get_location_from_ip(ip):
     g = geocoder.ip(ip)
-    pais = g.country
+    country = g.country
     city = g.city
-    return f"Country: {pais}\nCity: {city}"
+    return f"Country: {country}\nCity: {city}"
 
-def MostrarIP():
+def show_location():
     ip = entry.get()
-    location = LocalizacionIP(ip)
+    location = get_location_from_ip(ip)
     label.config(text=location)
 
 root = tk.Tk()
-root.title("Localizador IP")
+root.geometry('200x125')
+root.title("IP Geolocation")
 
-ip_label = tk.Label(root, text="IP:")
+ip_label = tk.Label(root, text="IP Address:")
 ip_label.pack()
 
 entry = tk.Entry(root)
 entry.pack()
 
-button = tk.Button(root, text="Mostrar", command=MostrarIP)
+button = tk.Button(root, text="Show Location", command=show_location)
 button.pack()
 
 label = tk.Label(root, text="")
